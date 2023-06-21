c95c299c08ee00eca9b737b76d92bf29b6d9f476
phylroy <phylroy.lopez@gmail.com>
2023-06-21 16:11:10 -0400

    experimental wsl2g usage

--- powershell/3-dockerfile_create_container.ps1
+++ powershell/3-dockerfile_create_container.ps1
@@ -1,7 +1,7 @@
 . .\env.ps1
 $container_name =  $Args[0]
 $command = docker
-$arguments = "docker create -ti -e DISPLAY=${x_display} -P -v c:/Users/${win_user}:${linux_home_folder}/windows-host --name ${container_name}  ${image}"
+$arguments = "docker container create -it -v c:/Users/${win_user}:${linux_home_folder}/windows-host -v /run/desktop/mnt/host/wslg/.X11-unix:/tmp/.X11-unix -v /run/desktop/mnt/host/wslg:/mnt/wslg -e DISPLAY=:0 -e WAYLAND_DISPLAY=wayland-0 -e XDG_RUNTIME_DIR=/mnt/wslg/runtime-dir -e PULSE_SERVER=/mnt/wslg/PulseServer --name ${container_name}  ${image}"
 Write-Host $arguments
 iex $arguments
 
--- powershell/env.ps1
+++ powershell/env.ps1
@@ -1,42 +1,8 @@
-$os_version = &git rev-parse --abbrev-ref HEAD 
+$os_version = "3.6.1"
 $image = "canmet/btap-development-environment:$($os_version)"
-$canmet_server_folder= "//s-bcc-nas2/Groups/Common\ Projects/HB/dockerhub_images/"
-$xfolder = ""
-$x_process= ""
-
-if (Test-Path "c:\Program Files\Xming" ) {
-   $xfolder = "c:\Program Files\Xming\Xming.exe"
-   $process = 'xming'
-}
-if (Test-Path "c:\Program Files(x86)\Xming" ) {
-   $xfolder = 'c:\Program Files(x86)\Xming\Xming.exe'
-   $process = 'xming'
-}
-if (Test-Path "C:\Program Files\VcXsrv" ) {
-
-   $xfolder = 'C:\Program Files\VcXsrv\vcxsrv.exe'
-   $process = 'vcxsrv'
-}
-
-$host_ip = 'host.docker.internal'
 $win_user = $env:UserName
-
-$is_x_server_running = Get-Process $process -ErrorAction SilentlyContinue  
-
-if($is_x_server_running -eq $null) {
-    $xmingexe = $xfolder
-	$arguments = '-ac -multiwindow -clipboard  -dpi 108'
-	Write-Host $xmingexe $arguments
-	start-process $xmingexe $arguments
-	}
-	
-
-$x_display = $host_ip + ':0.0'
 $linux_home_folder = '/home/osdev'
-
 Write-Host "Windows User:"  $win_user
-Write-Host "host_ip:" $host_ip
-Write-Host "X server IP:" $x_display
 Write-Host "image name:" $image
 Write-Host "linux home folder:"  $linux_home_folder
 	
